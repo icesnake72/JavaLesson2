720a7fc05cc24213bc8e2beb564bdd250776df55
icesnake72 <icesnake72@gmail.com>
2025-01-17 18:09:45 +0900

    second commit

--- src/basic/Lesson01.java
+++ src/basic/Lesson01.java
@@ -1,7 +1,14 @@
 package basic;
 
+import java.util.Scanner;
+
 public class Lesson01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "!");
+        sc.close();
     }
 }
